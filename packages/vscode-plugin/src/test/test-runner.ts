abc16247955226fee05ac48a17b1f920ea6e2f26
Jasper van der Veen <jasper.vanderveen99@gmail.com>
2026-01-17 23:53:22 +0100

    chore: update test runner and refactor launch configuration

---
+++ packages/vscode-plugin/src/test/test-runner.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import { glob } from 'glob';
+import Mocha from 'mocha';
+
+export async function run(): Promise<void> {
+  const testsRoot = __dirname;
+
+  const mocha = new Mocha({
+    ui: 'bdd',
+    rootHooks: {
+      beforeAll() {
+        import('./setup.js');
+      }
+    }
+  });
+
+  const testFiles = await glob('**/**.spec.js', { cwd: testsRoot });
+  testFiles.forEach(file => mocha.addFile(path.resolve(testsRoot, file)));
+
+  const failures = await new Promise<number>((resolve) => {
+    mocha.run((failures) => resolve(failures));
+  });
+  
+  if (failures > 0) {
+    throw new Error(`${failures} tests failed.`);
+  }
+}
